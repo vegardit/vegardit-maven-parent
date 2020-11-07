62da760a5a2a3d8d4ec3d467f6eb0d3aea4e4580
sebthom <sebthom@users.noreply.github.com>
2020-11-07 14:09:32 +0100

    Fix test compilation on Java8

--- .ci/release-trigger.sh
+++ .ci/release-trigger.sh
@@ -1,7 +1,7 @@
 # this file is evaluated by .ci/build.sh
 
-POM_CURRENT_VERSION="2.4.20-SNAPSHOT" # perform release if pom.xml matches this version
-POM_RELEASE_VERSION="2.4.20" # next release version
+POM_CURRENT_VERSION="2.4.21-SNAPSHOT" # perform release if pom.xml matches this version
+POM_RELEASE_VERSION="2.4.21" # next release version
 
 DRY_RUN=false # is dry run?
 SKIP_TESTS=true # skip tests?
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,7 +9,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 
-## [2.4.20] - 2020-11-07
+## [2.4.21] - 2020-11-07
 
 ### Changed
 - upgrade to Eclipse Java Compiler 3.23.0
