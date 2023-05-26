24113e45a14e9d8a7326e10f3e38b8a0daf3f912
sebthom <sebthom@users.noreply.github.com>
2023-05-26 16:12:07 +0200

    trigger release build

--- .ci/release-trigger.sh
+++ .ci/release-trigger.sh
@@ -1,7 +1,7 @@
 # this file is evaluated by .ci/build.sh
 
-POM_CURRENT_VERSION="3.0.13-SNAPSHOT" # perform release if pom.xml matches this version
-POM_RELEASE_VERSION="3.0.13" # next release version
+POM_CURRENT_VERSION="3.0.14-SNAPSHOT" # perform release if pom.xml matches this version
+POM_RELEASE_VERSION="3.0.14" # next release version
 
 DRY_RUN=false # is dry run?
 SKIP_TESTS=true # skip tests during release build?
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 
+## [3.0.14] - 2023-05-26
+
 ### Changed
 - Maven plugin upgrades:
   - build-helper-maven-plugin from 3.3.0 to 3.4.0
