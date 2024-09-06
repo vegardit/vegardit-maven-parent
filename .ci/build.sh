7885590f7aa83f52b585e2d514b933ab8f630dcf
sebthom <sebthom@users.noreply.github.com>
2024-09-06 11:40:49 +0200

    chore: format

--- .ci/build.sh
+++ .ci/build.sh
@@ -84,7 +84,6 @@ echo "###################################################"
 projectVersion=$(python -c "import xml.etree.ElementTree as ET; \
   print(ET.parse(open('pom.xml')).getroot().find(  \
   '{http://maven.apache.org/POM/4.0.0}version').text)")
-
 echo "  -> Current Version: $projectVersion"
 
 chmod u+x ./mvnw
