979536a05c1f599803de43ad453dfb2b530b2dbf
sebthom <sebthom@users.noreply.github.com>
2025-10-12 18:35:07 +0200

    ci: trigger release build

--- .ci/release-trigger.sh
+++ .ci/release-trigger.sh
@@ -3,8 +3,8 @@
 
 # this file is evaluated by shared workflow https://github.com/sebthom/gha-shared/blob/v1/.github/workflows/maven-build.yml
 
-POM_CURRENT_VERSION="6.1.12-SNAPSHOT" # perform release if pom.xml matches this version
-POM_RELEASE_VERSION="6.1.12" # next release version
+POM_CURRENT_VERSION="6.1.13-SNAPSHOT" # perform release if pom.xml matches this version
+POM_RELEASE_VERSION="6.1.13" # next release version
 
 DRY_RUN=false # is dry run?
 SKIP_TESTS=true # skip tests during release build?
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 
+## [6.1.13] - 2025-10-12
+
 ### Changed
 
 - Maven plugin upgrades:
